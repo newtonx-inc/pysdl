55ed55e384219af5d5e8d439084eac16ad422806
joshelbahrawy <josh.elbahrawy@newtonx.com>
2022-10-20 11:13:14 -0400

    upgraded dependency and bumped minor version up

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='pysdl',
-    version='1.0.3',
+    version='1.1.0',
     url='',
     license='',
     author='NewtonX',
